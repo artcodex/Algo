a8e7313ea2b9964716a22ebfe1b2f8c6ec5715ce
Avi Brenner <abrenner@salesforce.com>
2023-01-31 13:57:35 -0700

    Sales match

---
+++ Python/sales_by_match.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs = {}
+    count = 0
+    
+    for i in ar:
+        if i in pairs:
+            pairs[i]+=1
+        else:
+            pairs[i]=1
+    
+    for v in pairs.values():
+        count += math.floor(v / 2)
+            
+    return int(count)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
